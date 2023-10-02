429ba5b2761fd6aef9642b41c0138cb2e209ef0c
tbjagodits <tbjagodits@gmail.com>
2023-10-02 09:09:05 -0700

    progress

--- src/editor.rs
+++ src/editor.rs
@@ -1,9 +1,16 @@
 use crate::Terminal;
 use termion::event::Key;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+struct Position {
+    x: usize,
+    y: usize,
+}
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
+    cursor_position: Position,
 }
 
 impl Editor {
@@ -32,6 +39,7 @@ impl Editor {
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("failed to initialize terminal"),
+            cursor_position: Position { x: 0, y: 0 }
         }
     }
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
@@ -48,10 +56,24 @@ impl Editor {
         Terminal::cursor_show();
         Terminal::flush()
     }
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("hecto editor -- version {}\r", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        println!("{}\r", welcome_message);
+    }
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height - 1 {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
             Terminal::clear_current_line();
-            println!("~\r");
+            if row == height /3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r")
+            }
         }
     }
 }
